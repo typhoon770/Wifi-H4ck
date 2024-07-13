57c39e010f7e17c7d205ef3a367390b016579cc5
Typhoon <164783637+typhoon770@users.noreply.github.com>
2024-07-13 15:38:03 +0600

    witux.py

--- witux.py
+++ witux.py
@@ -1,5 +1,5 @@
 #----ভাই কি আমার স্ক্রিপ্ট চুরি করতে আইছেন😂
 import os
-os.system("xdg-open https://t.me/echo_default")
+os.system("xdg-open https://t.me/echo_typhoon")
 import witux
 witux.loging()
